89cd2aec70c5000de67a52267530854013fe5ab7
unknown <jakub_nowak2000@o2.pl>
2022-04-20 21:11:41 +0200

    Added reading png files, converting to gray scale, returning input layer, libpng does not build properly

--- example/main.cpp
+++ example/main.cpp
@@ -1,8 +1,125 @@
 #include <biai/network.h>
 #include <png.h>
 
+#include <Eigen/Dense>
+
+#include <iostream>
+
 using namespace biai;
-int main(int argc, char **argv)
+
+class PNG
 {
-	return 0;
+  public:
+    bool readPngFileAsGrayScale(std::string filename);
+    Eigen::VectorXf networkInputLayer();
+
+  private:
+    int width, height;
+    png_byte color_type;
+    png_byte bit_depth;
+    png_bytep* row_pointers = nullptr;
+};
+
+int main(int argc, char** argv)
+{
+    PNG png;
+    png.readPngFileAsGrayScale("/data/Circle_3a9c6616-2a8c-11ea-8123-8363a7ec19e6.png");
+    std::cout << png.networkInputLayer();
+    return 0;
+}
+
+Eigen::VectorXf PNG::networkInputLayer()
+{
+    Eigen::VectorXf v(width * height);
+    for(int y = 0; y < height; y++)
+    {
+        png_bytep row = row_pointers[y];
+        for(int x = 0; x < width; x++)
+        {
+            png_bytep px      = &(row[x]);
+            float normalizedX = px[0] >> 8; // make range 0..255 to 0..1
+            v << normalizedX;
+        }
+    }
+    return v;
+}
+
+bool PNG::readPngFileAsGrayScale(std::string filename)
+{
+    FILE* file = fopen(filename.c_str(), "rb");
+
+    png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if(!png)
+        return false;
+
+    png_infop info = png_create_info_struct(png);
+    if(!info)
+        return false;
+
+    if(setjmp(png_jmpbuf(png)))
+        return false;
+
+    png_init_io(png, file);
+
+    png_read_info(png, info);
+
+    width      = png_get_image_width(png, info);
+    height     = png_get_image_height(png, info);
+    color_type = png_get_color_type(png, info);
+    bit_depth  = png_get_bit_depth(png, info);
+
+    if(bit_depth == 16)
+        png_set_strip_16(png);
+
+    if(color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
+        png_set_expand_gray_1_2_4_to_8(png);
+
+    if(png_get_valid(png, info, PNG_INFO_tRNS))
+        png_set_tRNS_to_alpha(png);
+
+    // Fill alpha channel
+    if(color_type == PNG_COLOR_TYPE_RGB || color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_PALETTE)
+        png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
+
+    // CONVERT TO GRAY SCALE
+    //
+    //  error_action = 1: silently do the conversion
+    //     error_action = 2: issue a warning if the original
+    //                       image has any pixel where
+    //                       red != green or red != blue
+    //     error_action = 3: issue an error and abort the
+    //                       conversion if the original
+    //                       image has any pixel where
+    //                       red != green or red != blue
+    //
+    //     red_weight:       weight of red component times 100000
+    //     green_weight:     weight of green component times 100000
+    //                       If either weight is negative, default
+    //                       weights (21268, 71514) are used.
+    // http://www.libpng.org/pub/png/libpng-1.2.5-manual.html
+    if(color_type == PNG_COLOR_TYPE_RGB || color_type == PNG_COLOR_TYPE_RGB_ALPHA)
+        png_set_rgb_to_gray_fixed(png, 2, 21268, 71514);
+    else if(color_type == PNG_COLOR_TYPE_PALETTE)
+    {
+        png_set_palette_to_rgb(png);
+        png_set_rgb_to_gray_fixed(png, 2, 21268, 71514);
+    }
+
+    png_read_update_info(png, info);
+
+    if(row_pointers) abort();
+
+    row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * height);
+    for(int y = 0; y < height; y++)
+    {
+        row_pointers[y] = (png_byte*)malloc(png_get_rowbytes(png, info));
+    }
+
+    png_read_image(png, row_pointers);
+
+    fclose(file);
+
+    png_destroy_read_struct(&png, &info, NULL);
+
+    return true;
 }
